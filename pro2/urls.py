9e849771c4d4de3881d427505f5c844b973d56fa
Rishabh <rishabhgarg313@gmail.com>
2021-04-21 16:24:05 +0530

    Changes made to make it live

--- pro2/urls.py
+++ pro2/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home , name = "home"),
-    path('analyze', views.analyze , name = "removepunc")
+    path('analyze', views.analyze , name = "removepunc"),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     # path('removepunc', views.removepunc , name = "removepunc"),
     # path('capfirst', views.capfirst , name = "capfirst"),
     # path('newlinremove', views.newlinremove , name = "newlinremove"),
