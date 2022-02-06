e1f41a77d3755edf8e2dc9a689c6e600d62b0846
Rishabh-DS <79005134+Rishabh-DS@users.noreply.github.com>
2022-02-06 11:18:43 +0530

    Update settings.py

--- pro2/settings.py
+++ pro2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#)qlijdyt+=(o0-0r8-#npgh^em30^jjocwg75%giyrrurwns*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1' , 'text-utilities.herokuapp.com']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
